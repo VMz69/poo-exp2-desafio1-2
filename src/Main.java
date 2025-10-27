fb36a1d3c6b627a4516bad38b3a3e25aa51950f8
miltonayala <miltonayala1704@gmail.com>
2025-10-27 02:01:55 -0600

    Comment added

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,15 @@ import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-//        SwingUtilities.invokeLater(() -> new VentanaPrincipal());
+        try {
+            // SwingUtilities.invokeLater(() -> new VentanaPrincipal());
+            // para forzar el tema por defecto de swing e ignorar los estilos de macOS
+            UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         SwingUtilities.invokeLater(() -> new _MainFrame());
     }
 }
+
+//Validar con los compañeros para que es VentanaPrincipal()
